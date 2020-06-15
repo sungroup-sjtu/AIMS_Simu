bf51dce5e721d2f879800435d256facd24a43a72
XiangyanSJTU <hnxxxy123@sjtu.edu.cn>
2020-06-15 05:24:53 -0400

    small update.

--- database/NISTDBModify.py
+++ database/NISTDBModify.py
@@ -1,15 +1,19 @@
+import os
 import sys
-sys.path.append('..')
+CWD = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(CWD, '..'))
 from app import create_app
 from app.models_nist import *
 from app.selection import ClassificationAtomType
 from rdkit.Chem import AllChem as Chem
-from mstools.smiles.smiles import has_stereo_isomer_from_inchi
+from mstools.smiles.smiles import is_accurate_inchi
 procedure = 'npt'
 app = create_app(procedure)
 app.app_context().push()
-molecules = NistMolecule.query.filter(NistMolecule.id < 85)
-classifier = ClassificationAtomType(AllowedAtomicNum=[1, 5, 6, 7, 8, 9, 14, 15, 16, 17, 35, 53])
+molecules = NistMolecule.query
+classifier = ClassificationAtomType(
+    AllowedAtomicNum=[1, 5, 6, 7, 8, 9, 14, 15, 16, 17, 35, 53]
+)
 for i, mol in enumerate(molecules):
     sys.stdout.write('\r%i / %i. %s\t\t\t\t' % (i, molecules.count(), mol.smiles))
     rdk_mol = Chem.MolFromInchi(mol.inchi)
@@ -19,8 +23,8 @@ for i, mol in enumerate(molecules):
     elif rdk_mol is None:
         mol.remark = 'unrecognizable'
     elif classifier.classify(mol.inchi):
-        if has_stereo_isomer_from_inchi(mol.inchi):
-            mol.remark = 'has undefined stereo'
-        else:
+        if is_accurate_inchi(mol.inchi):
             mol.remark = 'selected'
+        else:
+            mol.remark = 'non-accurate inchi'
     db.session.commit()
